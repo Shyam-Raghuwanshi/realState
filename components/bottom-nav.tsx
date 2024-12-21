0dfc68bd3f30cc98c36398ec6b91c788c95c88a4
Notedown7272 <shivang6676@gmail.com>
2024-12-21 14:16:00 +0530

    UI changes 3

--- components/bottom-nav.tsx
+++ components/bottom-nav.tsx
@@ -1,20 +1,20 @@
-import { Heart, Search, User2 } from "lucide-react"
+import { Heart, Search, User2 } from "lucide-react";
 
 export default function BottomNav() {
-    return (
-        <div className="w-full fixed py-2 bottom-0 z-[999999] md:hidden flex justify-center space-x-12 bg-white">
-            <div className="flex flex-col space-y-1 items-center justify-center">
-                <Search className="h-6 w-6"/>
-                <span className="text-xs">Explore</span>
-            </div>
-            <div className="flex flex-col space-y-1 items-center justify-center">
-                <Heart className="h-6 w-6"/>
-                <span className="text-xs">WishList</span>
-            </div>
-            <div className="flex flex-col space-y-1 items-center justify-center">
-                <User2 className="h-6 w-6"/>
-                <span className="text-xs">Log in</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full fixed py-2 bottom-0 z-[999999] md:hidden flex justify-center space-x-12 bg-white">
+      <div className="flex flex-col space-y-1 items-center justify-center">
+        <Search className="h-5 w-6" />
+        <span className="text-xs">Explore</span>
+      </div>
+      <div className="flex flex-col space-y-1 items-center justify-center">
+        <Heart className="h-6 w-6" />
+        <span className="text-xs">WishList</span>
+      </div>
+      <div className="flex flex-col space-y-1 items-center justify-center">
+        <User2 className="h-6 w-6" />
+        <span className="text-xs">Log in</span>
+      </div>
+    </div>
+  );
 }
